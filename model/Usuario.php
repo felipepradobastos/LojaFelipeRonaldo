6e18ca3b4dc42e48e2f7b0fd527224827489ff91
Felipe Bastos <felipepradobastos@gmail.com>
2020-01-25 14:23:57 -0300

    teste

---
+++ model/Usuario.php
@@ -0,0 +1,12 @@
+<?php
+
+class Usuario
+{
+    private $id;
+    private $nome;
+    private $email;
+    private $senha;
+    private $documento;
+    private $telefone;
+
+}
---
+++ register.php
@@ -0,0 +1,32 @@
+<head>
+    <meta charset="UTF-8">
+    <title>E-commerce Brothers</title>
+
+    <script type="text/javascript" src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <script type="text/javascript" src="src/js/index.js"> </script>
+    <link rel="stylesheet" href="src/css/default.css">
+    <link rel="stylesheet" href="src/css/index.css">
+</head>
+
+<header>
+    <!--    Dropdown-->
+    <ul id="dropcategory" class="dropdown-content">
+        <li><a href="#!">PC</a></li>
+        <li><a href="#!">Playstation</a></li>
+        <li><a href="#!">XBOX</a></li>
+    </ul>
+    <nav>
+        <div class="nav-wrapper grey darken-1" >
+            <a href="#" class="brand-logo right">Logo</a>
+            <ul id="nav-mobile" class="left hide-on-med-and-down">
+                <li><a><i class="material-icons">home</i></a></li>
+                <li><a class="dropdown-trigger" href="#!" data-target="dropcategory">Produtos<i class="material-icons right">arrow_drop_down</i></a></li>
+                <li><a href="register.php">Cadastro</a></li>
+            </ul>
+        </div>
+    </nav>
+</header>
